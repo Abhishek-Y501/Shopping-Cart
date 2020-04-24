6d607fefa20e021e14daab7f326838d19ac1b5de
Abhishek-Y501 <abhikrhans@gmail.com>
2020-04-24 09:34:40 +0530

    added loaderr

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,15 +4,17 @@ import { catchError, map } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
 
 import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   public User = new BehaviorSubject<any>(false);
+  public setTimer;
 
   private rootUrl = environment.apiBaseUrl;
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   signUp(user: any) {
     let newUser = { Name: user.Name, Email: user.Email, Password: user.Password, Cart: [] };
@@ -30,10 +32,28 @@ export class AuthService {
       catchError(this.handleError),
       map((response: any) => {
         this.User.next(true);
+        localStorage.setItem('UD', JSON.stringify({ Email: user.Name, Password: user.Password }));
         localStorage.setItem('user', JSON.stringify({ token: response.data.token, expireIn: response.data.expiresIn }));
+        this.autoLogout(response.data.expiresIn * 1000);
         return response;
       })
     )
+  }
+
+  autoLogin() {
+    const user = JSON.parse(localStorage.getItem('UD'));
+    if (user) {
+      this.router.navigate(['/user/products']);
+    } else {
+      this.logout();
+      this.router.navigate(['/']);
+    }
+  }
+
+  autoLogout(expireIn: any) {
+    this.setTimer = setTimeout(() => {
+      this.logout();
+    }, expireIn)
   }
 
   logout() {
